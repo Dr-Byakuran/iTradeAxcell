f30c3ba58a6e65a9574286e0bdda69af5f90444d
cjs <cjs@qq.com>
2017-12-25 17:28:52 +0800

    添加菜单TutorRate的Controller

---
+++ iTrade/Controllers/TutorRateController.cs
@@ -0,0 +1,27 @@
+﻿using iTrade.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iTrade.Controllers
+{
+    public class TutorRateController : Controller
+    {
+        private StarDbContext db = new StarDbContext();
+        // GET: TutorRate
+        public ActionResult Index(string txtFilter)
+        {
+            var result = db.Tutors.Take(200).ToList();
+
+            if (!string.IsNullOrEmpty(txtFilter))
+            {
+                result = db.Tutors.Where(x => x.TutorName.Contains(txtFilter) || x.TutorCode.StartsWith(txtFilter)).Take(200).ToList();
+            }
+
+            return View(result);
+        }
+    }
+}
