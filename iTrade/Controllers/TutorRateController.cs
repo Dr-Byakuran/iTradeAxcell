ff26c9afa9318d5dfe1df5f278983645ab57d9a9
cjs <cjs@qq.com>
2017-12-26 14:32:08 +0800

    添加TutorRate的Controller的方法

--- iTrade/Controllers/TutorRateController.cs
+++ iTrade/Controllers/TutorRateController.cs
@@ -23,5 +23,59 @@ namespace iTrade.Controllers
 
             return View(result);
         }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutor stu = db.Tutors.Find(id);
+            ViewBag.No = stu.AttId;
+            if (stu == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Status = "GeneralInfo";
+            ViewBag.CustNo = id;
+            ViewData["CourseAll"] = db.Courses.Where(x => x.IsActive == true).ToList();
+            return View(stu);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var p = new List<TutorRate>();
+            p = db.TutorRates.Where(x => x.TutorID == id).ToList();
+
+            return PartialView(p);
+        }
+
+        public ActionResult Item(int id)
+        {
+            Tutor tut = db.Tutors.Find(id);
+            TutorRate p = new TutorRate();
+            p.TutorID = tut.TutorID;
+            p.TutorName = tut.TutorName;
+            p.TutorType = tut.TutorType;
+
+            return PartialView(p);
+        }
+
+        public void AddItem(TutorRate det)
+        {
+            var tutorRate = db.TutorRates.Where(m => m.TutorID == det.TutorID).ToList();
+            double tuCount = tutorRate.Count;
+            det.Position = tuCount + 1;
+
+            db.TutorRates.Add(det);
+            int x = db.SaveChanges();
+            int p = det.TutorID;
+
+            if (x != 0)
+            {
+                Response.Redirect("Edit/" + p);
+            }
+
+        }
     }
 }
