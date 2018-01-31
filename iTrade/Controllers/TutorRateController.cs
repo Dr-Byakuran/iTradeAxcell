ccb73477199d7d94f67f4938acefd3e52dfcc642
cjs <cjs@qq.com>
2018-01-31 13:46:44 +0800

    调整，TutorRate,的页面，删除不必要的东西

--- iTrade/Controllers/TutorRateController.cs
+++ iTrade/Controllers/TutorRateController.cs
@@ -33,91 +33,25 @@ namespace iTrade.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Tutor stu = db.Tutors.Find(id);
-            ViewBag.No = stu.AttId;
             if (stu == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.Status = "GeneralInfo";
-            ViewBag.CustNo = id;
-            ViewData["CourseAll"] = db.Courses.Where(x => x.IsActive == true).ToList();
+
             return View(stu);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Tutor tu)
-        {
-            int tutorID = 0;
-            if (ModelState.IsValid)
-            {
-                tutorID = tu.TutorID;
-                tu.TutorType = (Request["TutorType"] == null) ? "" : Request["TutorType"].ToString();
-                tu.JobType = (Request["JobType"] == null) ? "" : Request["JobType"].ToString();
-                tu.Gender = (Request["Gender"] == null) ? "" : Request["Gender"].ToString();
-
-                tu.Subjects = (Request["CourseSubjects"] != null) ? Request["CourseSubjects"].ToString() : "";
-                string zhi = "";
-                string categoryid = "";
-                List<Course> cse = db.Courses.Where(x => x.IsActive == true).ToList();
-                string[] val = tu.Subjects.Split(',');
-                for (int i = 0; i < val.Length; i++)
-                {
-                    for (int k = 0; k < cse.Count; k++)
-                    {
-                        if (val[i] == cse[k].CourseID.ToString())
-                        {
-                            if (zhi == "")
-                            {
-                                zhi = cse[k].CourseName;
-                            }
-                            else
-                            {
-                                zhi += "," + cse[k].CourseName;
-                            }
-
-                            //if (categoryid == "")
-                            //{
-                            //    categoryid = cse[k].CourseCategory;
-                            //}
-                            //else
-                            //{
-                            //    categoryid += "," + cse[k].CourseCategory;
-                            //}
-                            break;
-                        }
-                    }
-                }
-                tu.AttId = (Request["AttachmenNo"] == null) ? 0 : Convert.ToInt32(Request["AttachmenNo"].ToString());
-                tu.SubjectsName = zhi;
-                tu.CategoryID = categoryid;
-
-                tu.ModifiedBy = User.Identity.Name;
-                tu.ModifiedOn = DateTime.Now;
-
-                db.Entry(tu).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.CustNo = tutorID;
-
-            //ViewBag.Status = Request.Form["selectedTab"];
-            ViewData["CourseAll"] = db.Courses.Where(x => x.IsActive == true).ToList();
-            return View(tu);
-        }
-
-        public ActionResult Detail(string id)
+        public ActionResult Detail(int id)
         {
             var p = new List<TutorRate>();
-            p = db.TutorRates.Where(x => x.TutorCode == id).ToList();
+            p = db.TutorRates.Where(x => x.TutorID == id).ToList();
 
             return PartialView(p);
         }
 
-        public ActionResult Item(string id)
+        public ActionResult Item(int id)
         {
-            Tutor tut = db.Tutors.Where(x =>x.TutorCode ==id).FirstOrDefault();
+            Tutor tut = db.Tutors.Where(x =>x.TutorID ==id).FirstOrDefault();
             TutorRate p = new TutorRate();
             p.TutorID = tut.TutorID;
             p.TutorCode = tut.TutorCode;
